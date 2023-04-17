5015f05543a657f82cb8210e1d9d0c71a5b4959d
MikalViga <mikviga@gmail.com>
2023-04-17 16:50:08 +0200

    Fix diamondprint

--- my_code/parameters.py
+++ my_code/parameters.py
@@ -12,20 +12,25 @@ epsilon_decay = 0.95
 learning_rate = 0.001
 num_layers = 3
 num_neurons = 64
-activation_function = 'relu'
+activation_function = 'relu' #relu, tanh, sigmoid, selu, elu, softplus, softsign, hard_sigmoid, linear
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate) #SGD, Adagrad, Adadelta, Adam, Adamax, Nadam, RMSprop
 
-
-#RL training
-save_interval = 50
+#MCTS
 actual_games = 1000
 search_seconds = 5
+
+#RL training
+num_cached_anets = 5
+save_interval = 50 #actual_games/num_cached_anets
 max_buffer_size = 512
 
-#Simulation
+
+#other
 num_cached_anets = 5
-num_games = 75
-topp_models_filenames = ["c950_4.h5", "c400_4.h5", "c100_4.h5", "c0_4.h5"]
+
+#Simulation
+num_games = 1
+topp_models_filenames = ["c850_4.h5", "d850_5.h5"]
 thinking_depth = 40
 
 
